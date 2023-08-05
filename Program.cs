fb7daa94af0269e6be3953d4f5b5c814ee80fff1
ADiemus <genek14@mail.ru>
2023-08-06 00:58:55 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/* Написать программу, 
+которая из имеющегося массива строк 
+формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам.
+Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. */
+
+
+Console.WriteLine("Привет!");
+Console.WriteLine("Я учусь искать трехсимвольные строки и строить из них массивы.");
+Console.Write("Введи несколько слов или цифр, чтобы мы проверили, что у меня получается: ");
+
+string strings = Console.ReadLine();
+string[] stringArray = strings.Split(' ');
+
+
+// int size = stringArray.Length;
+// Console.WriteLine($"{String.Join(", ",stringArray)} "); 
+// Console.WriteLine($"{size} "); 
