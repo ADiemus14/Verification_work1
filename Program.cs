c00ef6cb2c50d81c590c54f02404b7bafe067280
ADiemus <genek14@mail.ru>
2023-08-06 01:20:12 +0300

    check_el

--- Program.cs
+++ Program.cs
@@ -17,3 +17,25 @@ string[] stringArray = strings.Split(' ');
 // int size = stringArray.Length;
 // Console.WriteLine($"{String.Join(", ",stringArray)} "); 
 // Console.WriteLine($"{size} "); 
+
+// проверка и учет элементов
+string [] CheckingElements(string [] Array)
+{
+  int m = 0;
+  for (int i = 0;i<Array.Length;i++)
+  {
+    if(Array[i].Length <=3)
+    m++;
+  }
+  string [] rez = new string [m];
+  int j = 0;
+  for (int i = 0;i<Array.Length;i++)
+  {
+    if(Array[i].Length <=3)
+    {
+        rez[j] = Array[i];
+        j++;
+    }
+  }
+  return rez;
+}
